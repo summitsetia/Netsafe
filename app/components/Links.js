f0041597684445d43895f26648a2c0424f7799b0
sumitsetia <summitsetia@gmail.com>
2024-04-28 03:51:25 +1200

    Randomizer For Links

--- app/components/Links.js
+++ app/components/Links.js
@@ -2,47 +2,62 @@
 import React from 'react';
 import Image from 'next/image'
 import dataSheet from "./dataSheet";
+import Link from 'next/link'
 
-const Boxes = ({ text, image }) => {
+const Boxes = ({ headerText, image, mainText, url }) => {
     return (
         <div className="pr-28" id="more-component">
         <div>
+        <Link href={url}>
             <Image
             src={image}
             alt="Column Image"
             width={4000}
             height={6000}
              />
+        </Link>
         </div>
         <div className="text-center">
-            <p className='p-4'>{text}</p>
+            <h1 className='p-4'>{headerText}</h1>
         </div>
         <div className="flex items-center justify-center"> 
-            <button className="mx-auto p-4">hello</button> 
+            <p className="mx-auto p-4">{mainText}</p>
         </div>
     </div>
            
     );
 };
 
-export default function Links() {
+const Links = () => {
     const [link, setLink] = React.useState(dataSheet)
 
-    function updateLinks () {
-
+    function updateLinks() {
+        const shuffledDataSheet = shuffleArray([...dataSheet]);
+        setLink(shuffledDataSheet);
     }
 
-    const boxElements = link.map(box => {
-        return <Boxes key={box.id} text={box.text} image={box.img} url={box.url}/>;
-    });
+    const shuffleArray = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
 
     return (
         <div>
             <div>
                 <button className="text-8xl" onClick={updateLinks}>Random</button>
             </div>
-            <div className="flex flex-row ml-48 ">{boxElements}</div>
+            <div className="flex flex-row ml-48 ">
+                <Boxes image={link[0].img} headerText={link[0].headerText} mainText={link[0].mainText} url={link[0].url} />
+                <Boxes image={link[1].img} headerText={link[1].headerText} mainText={link[1].mainText} url={link[1].url} />
+                <Boxes image={link[2].img} headerText={link[2].headerText} mainText={link[2].mainText} url={link[2].url} />
+                <Boxes image={link[3].img} headerText={link[3].headerText} mainText={link[3].mainText} url={link[3].url} />
+    
+            </div>
         </div>
     );
 }
- 
+
+export default Links;
--- app/components/dataSheet.js
+++ app/components/dataSheet.js
@@ -1,33 +1,38 @@
 const dataSheet = [
     {
         id: 1,
-        url: "",
-        img: "/images/vertical.jpg",
-        text: "hello"
+        url: "https://www.maono.com/en-fr/blogs/news/how-to-balance-gaming-and-life-1",
+        img: "/images/dataimage1.jpg",
+        headerText: "How to Balance Gaming and Life?",
+        mainText:"Looking At A Few Ideas That Everyone Should Keep in Mind While Gaming"
     },
     {
         id: 2,
-        url: "",
-        img: "/images/vertical.jpg",
-        text: "hi"
+        url: "https://ditchthelabel.org/7-ways-to-find-a-healthy-gaming-life-balance",
+        img: "/images/dataimage2.jpg",
+        headerText: "7 Ways to Find A Healthy Gaming-Life Balance",
+        mainText:"We All Enjoy Gaming Into the Night, But It Leads Us to Ignore Other Aspects Of Life"
     },
     {
         id: 3,
-        url: "",
-        img: "/images/vertical.jpg",
-        text: "hey"
+        url: "https://kotaku.com/how-to-balance-video-games-with-real-life-5990380",
+        img: "/images/dataimage3.jpg",
+        headerText: "How To Balance Video Games With Real Life",
+        mainText:"You Never Have Time For Video Games When Growing Up So It's Time To Make Comprimises"
     },
     {
         id: 4,
-        url: "",
-        img: "/images/vertical.jpg",
-        text: "wsg"
+        url: "https://oneboardfamily.com/getting-gaming-life-balance-right/",
+        img: "/images/dataimage4.jpg",
+        headerText: "Getting the Gaming-Life Balance Right",
+        mainText:"If You Want To Have A Great Time Gaming And Enjoying Life In The Real World, Too, Then You Need To Get Your Gaming-Life Balance Right"
     },
     {
         id: 5,
-        url: "",
-        img: "/images/vertical.jpg",
-        text: "howdy"
+        url: "https://www.medicalnewstoday.com/articles/318345",
+        img: "/images/dataimage5.jpg",
+        headerText: "How video games affect the brain",
+        mainText:"Video Gamers Spend Billions Of Hours Per Week In Front Of Their Screens, Here Is How They Affect The Brain"
     },
 ];
 
